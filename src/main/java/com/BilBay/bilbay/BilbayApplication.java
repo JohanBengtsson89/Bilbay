66e66d35aab86e74496489f0e2831b34202d3b1a
ChristofferWallberg <christoffer.wallberg85@gmail.com>
2023-02-03 09:47:39 +0100

    first commit

---
+++ src/main/java/com/BilBay/bilbay/BilbayApplication.java
@@ -0,0 +1,13 @@
+package com.BilBay.bilbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BilBay/bilbay/BilbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BilBay.bilbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
