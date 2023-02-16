3fed583283cf989750f6b6346bc9ca4e231b2437
unknown <johanbengtsson89@gmail.com>
2023-02-16 22:31:50 +0100

    Fixed after code review

--- src/main/java/com/BilBay/bilbay/models/BankPayment.java
+++ src/main/java/com/BilBay/bilbay/models/BankPayment.java
@@ -11,24 +11,27 @@ public class BankPayment {
     private int id;
 
     @Column(name = "users_id")
-    @OneToOne
-    @JoinColumn(name = "users_id, ", referencedColumnName = "id")
+    @ManyToOne(cascade = CascadeType.ALL)
     private User user;
 
     @Column(name = "bank_name")
     private String bankName;
 
     @Column(name = "bank_account_nr")
-    private String bankAccountNr;
+    private Long bankAccountNr;
+
+    @OneToMany(mappedBy = "bankPayment")
+    private List < PaymentTransaction > paymentTransactions = new ArrayList<>();
 
     public BankPayment() {
     }
 
-    public BankPayment(int id, User user, String bankName, String bankAccountNr) {
+    public BankPayment(int id, User user, String bankName, Long bankAccountNr, List<PaymentTransaction> paymentTransactions) {
         this.id = id;
         this.user = user;
         this.bankName = bankName;
         this.bankAccountNr = bankAccountNr;
+        this.paymentTransactions = paymentTransactions;
     }
 
     public int getId() {
@@ -55,11 +58,19 @@ public class BankPayment {
         this.bankName = bankName;
     }
 
-    public String getBankAccountNr() {
+    public Long getBankAccountNr() {
         return bankAccountNr;
     }
 
-    public void setBankAccountNr(String bankAccountNr) {
+    public void setBankAccountNr(Long bankAccountNr) {
         this.bankAccountNr = bankAccountNr;
+    }
+
+    public List<PaymentTransaction> getPaymentTransactions() {
+        return paymentTransactions;
+    }
+
+    public void setPaymentTransactions(List<PaymentTransaction> paymentTransactions) {
+        this.paymentTransactions = paymentTransactions;
     }
 }
