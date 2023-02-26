1c358261935d62766184dde3153e369c08ff3efe
AlirezaHK91 <kasra_tehran101@hotmail.com>
2023-02-26 19:41:32 +0100

    created repository for search function

--- src/main/java/com/BilBay/bilbay/repositories/ProductRepository.java
+++ src/main/java/com/BilBay/bilbay/repositories/ProductRepository.java
@@ -2,13 +2,19 @@ package com.BilBay.bilbay.repositories;
 
 import com.BilBay.bilbay.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 @EnableJpaRepositories
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    @Query("SELECT p FROM Product p JOIN p.productSpecification s WHERE s.color LIKE CONCAT('%',:query, '%') " +
+            "or s.gear LIKE CONCAT ('%',:query, '%')" +
+            "or s.enginePower LIKE CONCAT ('%',:query, '%')" +
+            "or p.productName LIKE CONCAT ('%',:query, '%')" +
+            "or p.category LIKE CONCAT ('%',:query, '%')")
+    List<Product> searchProducts(String query);
 
 }
