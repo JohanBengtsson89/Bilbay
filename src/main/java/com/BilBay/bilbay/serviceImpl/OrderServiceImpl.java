991c4411abeabb22baf7f80de59c3da9fe7de286
JohanBengtsson89 <johanbengtsson89@gmail.com>
2023-11-19 16:40:51 +0100

    Added service interface and service implementation class

--- src/main/java/com/BilBay/bilbay/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/BilBay/bilbay/serviceImpl/OrderServiceImpl.java
@@ -1,4 +1,38 @@
 package com.BilBay.bilbay.serviceImpl;
 
-public class OrderServiceImpl {
+import com.BilBay.bilbay.models.Order;
+import com.BilBay.bilbay.repositories.OrderRepository;
+import com.BilBay.bilbay.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Order getOrder(Long id) {
+        return orderRepository.findById(id).get();
+    }
+
+    @Override
+    public Order updateOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public String deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+        return "Order has been deleted";
+    }
 }
