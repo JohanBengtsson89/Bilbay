5dcf8f3e04fd6d6e6a6b25219a62468ec2cab9e0
JohanBengtsson89 <johanbengtsson89@gmail.com>
2023-11-19 16:05:12 +0100

    Added Service interface and serviceimpl class

--- src/main/java/com/BilBay/bilbay/controllers/BidController.java
+++ src/main/java/com/BilBay/bilbay/controllers/BidController.java
@@ -1,32 +1,30 @@
 package com.BilBay.bilbay.controllers;
 
-import com.BilBay.bilbay.models.Auction;
 import com.BilBay.bilbay.models.Bid;
 import com.BilBay.bilbay.services.BidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/api/")
 public class BidController {
 
     @Autowired
-    BidService bidService;
+    private BidService bidService;
+
+    public BidController(BidService bidService) { this.bidService = bidService;}
 
     @PostMapping("bid")
-
     public Bid createBid(@RequestBody Bid bid) {
-        return bidService.createBid(bid);
+        return bidService.placeBid(bid);
     }
 
     @GetMapping("auth/bid/{id}")
     @PreAuthorize("hasRole('PRIVATE') or hasRole('COMPANY') or hasRole('ADMIN')")
     public Bid getBidById(@PathVariable Long id) {
-        return bidService.getBidByID(id);
+        return bidService.getBid(id);
     }
 
 //    @GetMapping("find-highestBid")
--- src/main/java/com/BilBay/bilbay/serviceImpl/BidServiceImpl.java
+++ src/main/java/com/BilBay/bilbay/serviceImpl/BidServiceImpl.java
@@ -1,4 +1,24 @@
 package com.BilBay.bilbay.serviceImpl;
 
-public class BidServiceImpl {
+import com.BilBay.bilbay.models.Bid;
+import com.BilBay.bilbay.repositories.BidRepository;
+import com.BilBay.bilbay.services.BidService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BidServiceImpl implements BidService {
+    private BidRepository bidRepository;
+    public BidServiceImpl(BidRepository bidRepository) {
+        this.bidRepository = bidRepository;
+    }
+
+    @Override
+    public Bid placeBid(Bid bid) {
+        return bidRepository.save(bid);
+    }
+
+    @Override
+    public Bid getBid(Long id) {
+        return bidRepository.findById(id).get();
+    }
 }
